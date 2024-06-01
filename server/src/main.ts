5ba7dbdbbdcf78cabd7fdf396870f2480a6cc288
findreamer <findreame@126.com>
2024-06-01 23:14:00 +0800

    feat: 增加helmet防常见漏洞功能

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,5 @@
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -7,9 +9,26 @@ async function bootstrap() {
 
   const config = app.get(ConfigService);
 
+  // 设置访问频率
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15分钟
+      max: 1000, // 限制15分钟内最多只能访问1000次
+    }),
+  );
+
   // 获取 api 访问前缀
   const prefix = config.get('app.prefix');
   app.setGlobalPrefix(prefix);
+
+  // web 安全，防常见漏洞
+  // 注意： 开发环境如果开启 nest static module 需要将 crossOriginResourcePolicy 设置为 false 否则 静态资源 跨域不可访问
+  app.use(
+    helmet({
+      crossOriginOpenerPolicy: { policy: 'same-origin-allow-popups' },
+      crossOriginResourcePolicy: false,
+    }),
+  );
 
   await app.listen(3000);
 }
