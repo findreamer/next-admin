c0f5d25efe34a6c99ba76aad85d350b1f9fcbe8f
findreamer <findreame@126.com>
2024-06-02 13:46:15 +0800

    feat: 增加swagger文档功能

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -30,6 +31,36 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  // 配置swragger
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Next-admin')
+    .setDescription('Next admin 文档接口')
+    .setVersion('2.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  // 项目依赖当前文档功能，最好不要改变当前地址
+  // 生产环境使用 nginx 可以将当前文档地址 屏蔽外部访问
+  SwaggerModule.setup(`${prefix}/swagger-ui`, app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Next-Admin API Docs',
+  });
+
+  //服务端口
+  const port = config.get<number>('app.port') || 8080;
+  await app.listen(port);
+
+  console.log(
+    `Next-Admin 服务启动成功 `,
+    '\n',
+    '服务地址:',
+    `http://localhost:${port}${prefix}/`,
+    '\n',
+    'swagger 文档地址 :',
+    `http://localhost:${port}${prefix}/swagger-ui/`,
+  );
 }
 bootstrap();
