a58a10515f782d641fe8272c8e94a6427a54a28e
findreamer <findreame@126.com>
2024-06-11 12:49:01 +0800

    feat: 完成新建岗位接口

--- server/src/module/post/post.controller.ts
+++ server/src/module/post/post.controller.ts
@@ -1,9 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto';
+import { AllowAnon } from '@app/common/decorators/allow-anon.decorator';
 
 @ApiTags('岗位管理')
 @Controller('system/post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @ApiOperation({
+    summary: '岗位管理-创建',
+  })
+  @ApiBody({
+    type: CreatePostDto,
+    required: true,
+  })
+  @Post()
+  @AllowAnon()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.create(createPostDto);
+  }
 }
--- server/src/module/post/post.service.ts
+++ server/src/module/post/post.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SysPostEntity } from './entities/post.entity';
+import { CreatePostDto } from './dto';
+import { ResultData } from '@app/common/utils';
 @Injectable()
 export class PostService {
   constructor(
     @InjectRepository(SysPostEntity)
     private readonly sysPostEntityRepository: Repository<SysPostEntity>,
   ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    const res = await this.sysPostEntityRepository.save(createPostDto);
+    return ResultData.success(res);
+  }
 }
