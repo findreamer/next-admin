2e0d96aeff3aed7d5e06b500513f7065e8183f9a
findreamer <findreame@126.com>
2024-06-11 12:40:13 +0800

    feat: postService注册repository

--- server/src/module/post/post.module.ts
+++ server/src/module/post/post.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { SysPostEntity } from './entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([SysPostEntity])],
   controllers: [PostController],
   providers: [PostService],
   exports: [],
--- server/src/module/post/post.service.ts
+++ server/src/module/post/post.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { SysPostEntity } from './entities/post.entity';
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(SysPostEntity)
+    private readonly sysPostEntityRepository: Repository<SysPostEntity>,
+  ) {}
+}
