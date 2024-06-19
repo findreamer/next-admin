699ae15f457b5eb76d06cce8fc6ad845f0c462bd
findreamer <findream@126.com>
2024-06-19 12:55:08 +0800

    feat: 增加部门创建和列表接口

--- server/src/module/system/dept/dept.controller.ts
+++ server/src/module/system/dept/dept.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
 import { DeptService } from './dept.service';
+import { CreateDeptDto, ListDeptDto } from './dto';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
-@Controller('sys/dept')
+@Controller('system/dept')
 export class DeptController {
   constructor(private readonly deptService: DeptService) {}
+
+  @ApiOperation({
+    summary: '部门管理-新建',
+  })
+  @ApiBody({
+    type: CreateDeptDto,
+    required: true,
+  })
+  @Post()
+  @HttpCode(200)
+  create(@Body() createDeptDto: CreateDeptDto) {
+    return this.deptService.create(createDeptDto);
+  }
+
+  @ApiOperation({
+    summary: '部门管理-列表',
+  })
+  @Get('/list')
+  findAll(@Query() query: ListDeptDto) {
+    return this.deptService.findAll(query);
+  }
 }
--- server/src/module/system/dept/dept.service.ts
+++ server/src/module/system/dept/dept.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SySDeptEntity } from './entities/dept.entity';
+import { Repository } from 'typeorm';
+import { CreateDeptDto, ListDeptDto } from './dto';
+import { ResultData } from '@app/common/utils';
 
 @Injectable()
-export class DeptService {}
+export class DeptService {
+  constructor(
+    @InjectRepository(SySDeptEntity)
+    private readonly sysyDeptEntityRep: Repository<SySDeptEntity>,
+  ) {}
+
+  async create(createDeptDto: CreateDeptDto) {
+    if (createDeptDto.parentId) {
+      const parent = await this.sysyDeptEntityRep.findOne({
+        where: {
+          deptId: createDeptDto.parentId,
+          delFlag: '0',
+        },
+        select: ['ancestors'],
+      });
+
+      if (!parent) {
+        return ResultData.fail(500, '父级部门不存在');
+      }
+      const ancestors = parent.ancestors
+        ? `${parent.ancestors},${createDeptDto.parentId}`
+        : `${createDeptDto.parentId}`;
+
+      Object.assign(createDeptDto, { ancestors });
+    }
+    const res = await this.sysyDeptEntityRep.save(createDeptDto);
+
+    return ResultData.success(res);
+  }
+
+  async findAll(query: ListDeptDto) {
+    const entity = this.sysyDeptEntityRep.createQueryBuilder('entity');
+    entity.where('entity.delFlag = :delFlag', { delFlag: '0' });
+
+    if (query.deptName) {
+      entity.andWhere(`entity.deptName LIKE "%${query.deptName}%"`);
+    }
+
+    if (query.status) {
+      entity.where(`entity.status = :status`, { status: query.status });
+    }
+
+    const res = await entity.getMany();
+    return ResultData.success(res);
+  }
+}
