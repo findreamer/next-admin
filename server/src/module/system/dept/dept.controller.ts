2c40b2876bde63ddbef468a636804c46a81cd69f
findreamer <findream@126.com>
2024-06-20 12:50:50 +0800

    feat: 部门增加详情、更新接口

--- server/src/module/system/dept/dept.controller.ts
+++ server/src/module/system/dept/dept.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { DeptService } from './dept.service';
-import { CreateDeptDto, ListDeptDto } from './dto';
-import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { CreateDeptDto, ListDeptDto, UpdateDeptDto } from './dto';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AllowAnon } from '@app/common/decorators/allow-anon.decorator';
 
+@ApiTags('部门管理')
 @Controller('system/dept')
 export class DeptController {
   constructor(private readonly deptService: DeptService) {}
@@ -16,6 +27,7 @@ export class DeptController {
   })
   @Post()
   @HttpCode(200)
+  @AllowAnon()
   create(@Body() createDeptDto: CreateDeptDto) {
     return this.deptService.create(createDeptDto);
   }
@@ -24,7 +36,24 @@ export class DeptController {
     summary: '部门管理-列表',
   })
   @Get('/list')
+  @AllowAnon()
   findAll(@Query() query: ListDeptDto) {
     return this.deptService.findAll(query);
+  }
+
+  @ApiOperation({
+    description: '部门管理-详情',
+  })
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.deptService.findOne(+id);
+  }
+
+  @ApiOperation({
+    description: '部门管理-更新',
+  })
+  @Put()
+  update(@Body() updateDeptDto: UpdateDeptDto) {
+    return this.deptService.update(updateDeptDto);
   }
 }
--- server/src/module/system/dept/dept.service.ts
+++ server/src/module/system/dept/dept.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SySDeptEntity } from './entities/dept.entity';
 import { Repository } from 'typeorm';
-import { CreateDeptDto, ListDeptDto } from './dto';
+import { CreateDeptDto, ListDeptDto, UpdateDeptDto } from './dto';
 import { ResultData } from '@app/common/utils';
 
 @Injectable()
@@ -49,6 +49,46 @@ export class DeptService {
     }
 
     const res = await entity.getMany();
+    return ResultData.success(res);
+  }
+
+  async findOne(deptId: number) {
+    const res = await this.sysyDeptEntityRep.findOne({
+      where: {
+        deptId,
+        delFlag: '0',
+      },
+    });
+    return ResultData.success(res);
+  }
+
+  async update(updateDeptDto: UpdateDeptDto) {
+    // 如果修改父级部门，则需要拿到父级部门的 祖籍列表
+    const { parentId } = updateDeptDto;
+    if (parentId && parentId != 0) {
+      const parent = await this.sysyDeptEntityRep.findOne({
+        where: {
+          deptId: parentId,
+          delFlag: '0',
+        },
+        select: ['ancestors'],
+      });
+
+      if (!parent) {
+        return ResultData.fail(500, '父级部门不存在');
+      }
+      const ancestors = parent.ancestors
+        ? `${parent.ancestors},${parentId}`
+        : `${parentId}`;
+      Object.assign(updateDeptDto, { ancestors });
+    }
+
+    const res = await this.sysyDeptEntityRep.update(
+      {
+        deptId: updateDeptDto.deptId,
+      },
+      updateDeptDto,
+    );
     return ResultData.success(res);
   }
 }
