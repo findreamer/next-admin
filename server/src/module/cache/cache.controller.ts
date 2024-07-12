65ad37d819dd25f20f2de5d84d1013d6e672fa2b
findreamer <findream@126.com>
2024-07-12 21:03:15 +0800

    feat: 更新缓存接口

--- server/src/module/cache/cache.controller.ts
+++ server/src/module/cache/cache.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CacheService } from './cache.service';
 
-@Controller('cache')
-export class CacheController {}
+@ApiTags('缓存管理')
+@Controller('monitor/cache')
+export class CacheController {
+  constructor(private readonly cacheService: CacheService) {}
+
+  @ApiOperation({
+    summary: '缓存监控信息',
+  })
+  @Get()
+  getInfo() {
+    return this.cacheService.getInfo();
+  }
+
+  @ApiOperation({
+    summary: '缓存列表',
+  })
+  @Get('/getNames')
+  getNames() {
+    return this.cacheService.getNames();
+  }
+
+  @ApiOperation({
+    summary: '键名列表',
+  })
+  @Get('/getKeys/:id')
+  getKeys(@Param('id') id: string) {
+    return this.cacheService.getKeys(id);
+  }
+}
--- server/src/module/cache/cache.service.ts
+++ server/src/module/cache/cache.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common';
-
+import { RedisService } from '@app/module/redis/redis.service';
+import { ResultData } from '@app/common/utils';
 @Injectable()
-export class CacheService {}
+export class CacheService {
+  constructor(private readonly redisService: RedisService) {}
+
+  private readonly caches = [
+    {
+      cacheName: 'login_tokens:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '用户信息',
+    },
+    {
+      cacheName: 'sys_config:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '配置信息',
+    },
+    {
+      cacheName: 'sys_dict:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '数据字典',
+    },
+    {
+      cacheName: 'captcha_codes:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '验证码',
+    },
+    {
+      cacheName: 'repeat_submit:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '防重提交',
+    },
+    {
+      cacheName: 'rate_limit:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '限流处理',
+    },
+    {
+      cacheName: 'pwd_err_cnt:',
+      cacheKey: '',
+      cacheValue: '',
+      remark: '密码错误次数',
+    },
+  ];
+  async getInfo() {
+    const info = await this.redisService.getInfo();
+    const dbSzize = await this.redisService.getDbSize();
+    const commandStats = await this.redisService.commandStats();
+    return ResultData.success({
+      info,
+      dbSzize,
+      commandStats,
+    });
+  }
+
+  async getNames() {
+    return ResultData.success(this.caches);
+  }
+
+  async getKeys(id: string) {
+    const data = await this.redisService.keys(id + '*');
+    return ResultData.success(data);
+  }
+}
